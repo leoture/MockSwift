907772e40ea421602d2787dcfc1b038a272e59e9
jordhan leoture <jordhan.leoture@gmail.com>
2019-08-15 19:02:36 +0200

    begin readme

--- MockSwiftPlayground/Playground.playground/Contents.swift
+++ MockSwiftPlayground/Playground.playground/Contents.swift
@@ -1,4 +1,51 @@
-import UIKit
+struct User {
+  let name: String
+}
+
+protocol Service {
+  func function(identifier: String) -> User
+}
+
+import XCTest
 import MockSwift
 
-print("Hello world!")
+extension Mock: Service where WrappedType == Service {
+  func function(identifier: String) -> User { mocked(identifier) }
+}
+
+extension MockGiven where WrappedType == Service {
+  func function(identifier: Predicate<String>) -> Mockable<User> { mockable(identifier) }
+}
+
+extension MockThen where WrappedType == Service {
+  func function(identifier: Predicate<String>) -> Verifiable<User> { verifiable(identifier) }
+}
+
+extension Predicate {
+  static func not(_ keyPath: KeyPath<Input, Bool>) -> Predicate<Input> {
+    .match { !$0[keyPath: keyPath]}
+  }
+}
+
+class MyTests: XCTestCase {
+  @Mock private var service: Service
+
+  func test() {
+    // Given
+    let expectedUser = User(name: "John")
+
+    given(_service)
+      .function(identifier: .not(\.isEmpty))
+      .willReturn(expectedUser)
+
+    // When
+    let john = service.function(identifier: "id")
+
+    // Then
+    then(_service)
+      .function(identifier: .any)
+      .called()
+  }
+}
+
+MyTests.defaultTestSuite.run()
