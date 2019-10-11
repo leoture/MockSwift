ab3f62c4b1330324fd252bb7aa873108072126c4
jordhan leoture <jordhan.leoture@gmail.com>
2019-10-11 14:27:35 +0200

    fix colors makefile output

--- .xcmake/src/description.swift
+++ .xcmake/src/description.swift
@@ -1,11 +1,16 @@
 #!/usr/bin/swift
 
+let bleu = "\u{001B}[0;34m"
+let black = "\u{001B}[0;0m"
+
 let titleInput = CommandLine.arguments[1]
-let title = "== 🤖  \(titleInput)  =="
+let title = "==  \(titleInput)  =="
 let border = String(repeating: "=", count: title.count)
 
-let color = "\u{001B}[0;34m"
-let end = "\u{001B}[0;0m"
-print(color + border)
-print(title)
-print(border + end)
+func prettyPrint(_ msg: String) {
+  print(bleu + msg + black)
+}
+
+prettyPrint(border)
+prettyPrint(title)
+prettyPrint(border)
