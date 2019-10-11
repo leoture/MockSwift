02bcb104d8f04fefb374f2b2e39be80e25af3f7e
jordhan leoture <jordhan.leoture@gmail.com>
2019-10-11 16:58:40 +0200

    detect all XCMAKE_PLUGINS

--- .xcmake/core.mk
+++ .xcmake/core.mk
@@ -1,14 +1,13 @@
-XCMAKE_PLUGINS = description
-
 XCMAKE_PATH := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 XCMAKE_BIN_PATH := $(XCMAKE_PATH)bin
 XCMAKE_SRC_PATH := $(XCMAKE_PATH)src
 PATH := $(XCMAKE_BIN_PATH):$(PATH)
 
-XCMAKE_PLUGINS_BIN := $(addprefix $(XCMAKE_BIN_PATH)/, $(XCMAKE_PLUGINS))
+XCMAKE_PLUGINS :=  $(wildcard $(XCMAKE_SRC_PATH)/*.swift)
+XCMAKE_PLUGINS_BIN := $(patsubst $(XCMAKE_SRC_PATH)/%.swift, $(XCMAKE_BIN_PATH)/%, $(XCMAKE_PLUGINS))
 
 $(XCMAKE_PLUGINS_BIN): $(XCMAKE_BIN_PATH)/% : $(XCMAKE_SRC_PATH)/%.swift
-	mkdir -p $(XCMAKE_BIN_PATH)
-	swiftc $^ -o $@
+	@mkdir -p $(XCMAKE_BIN_PATH)
+	@swiftc $^ -o $@
 
 xcmake: $(XCMAKE_PLUGINS_BIN)
