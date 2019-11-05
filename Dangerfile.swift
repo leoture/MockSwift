4a4ce6eec24e953a20af776781c415ce45fa70cd
jordhan leoture <jordhan.leoture@gmail.com>
2019-11-05 18:24:34 +0100

    update Dangerfile.swift

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,1 +1,15 @@
-message("Hello, this worked")
+import Danger
+
+let danger = Danger()
+let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
+
+let changelogChanged = allSourceFiles.contains("CHANGELOG.md")
+let sourceChanges = allSourceFiles.first(where: { $0.hasPrefix("Sources") })
+
+if !changelogChanged && sourceChanges != nil {
+  warn("No CHANGELOG entry added.")
+}
+
+if danger.github.pullRequest.body.isEmpty {
+  warn("Please, provide a description to your PR.")
+}
