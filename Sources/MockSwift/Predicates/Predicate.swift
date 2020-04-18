6ad11b34dcdb5e04ba28d511bdb98caf723e42a3
Jordhan Leoture <jordhan.leoture@gmail.com>
2020-04-18 13:52:56 +0200

    fix coverage reporting

--- Sources/MockSwift/Predicates/Predicate.swift
+++ Sources/MockSwift/Predicates/Predicate.swift
@@ -50,7 +50,7 @@ public class Predicate<Input> {
   /// - Returns: A new `Predicate<Input>`.
   public static func match(description: String? = nil,
                            any type: Input.Type = Input.self,
-                           _ predicate: @escaping (Input) -> Bool) -> Predicate<Input> {
+                           when predicate: @escaping (Input) -> Bool) -> Predicate<Input> {
     Predicate(description: description ?? "a \(type)", predicate: predicate)
   }
 
@@ -59,11 +59,11 @@ public class Predicate<Input> {
   /// - Parameter type: The type to match.
   /// - Parameter keyPath: The keyPath that will be used to verify that the entry statisfies the Predicate.
   /// - Returns: A new `Predicate<Input>`.
-  public class func match(description: String? = nil,
+  /*public class func match(description: String? = nil,
                           any type: Input.Type = Input.self,
                           when keyPath: KeyPath<Input, Bool>) -> Predicate<Input> {
     .match(description: description ?? "a \(type)") { $0[keyPath: keyPath] }
-  }
+  }*/
 
   /// Creates a `Predicate<Input>` able to match any value of type `Input`.
   /// - Parameter type: The type to match.
